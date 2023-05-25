244dfa331f9a4c5bdbedd25ae8e81925493caf3c
Rakshit <rakshit@ad.infosys.com>
2023-05-26 00:07:06 +0530

    added top books

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/navbar/Navbar';
+import { ExploreTopBooks } from './homepage/ExploreTopBooks';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <ExploreTopBooks/>
     </div>
   );
 }
