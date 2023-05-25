f0b044c7a920975b17a8fdb18764b2819bf9823b
Rakshit <rakshit@ad.infosys.com>
2023-05-25 21:19:09 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my Books Library :-)</h1>
+      <Navbar/>
     </div>
   );
 }
