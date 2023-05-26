5124d925ada5fcfd3e727933cd382cc9606f7e12
Rakshit <rakshit@ad.infosys.com>
2023-05-26 10:07:59 +0530

    added return book

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/navbar/Navbar';
-import { ExploreTopBooks } from './homepage/ExploreTopBooks';
+import { ExploreTopBooks } from './layouts/homepage/ExploreTopBooks';
+import { Carousel } from './layouts/homepage/Carousel';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
