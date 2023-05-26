1490fae6c0f29a6fdd9e7539b6663745c7533677
Rakshit <rakshit@ad.infosys.com>
2023-05-26 12:06:29 +0530

    homepage cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/navbar/Navbar';
-import { ExploreTopBooks } from './layouts/homepage/ExploreTopBooks';
-import { Carousel } from './layouts/homepage/Carousel';
-import { Heros } from './layouts/homepage/Heros';
-import { LibraryServices } from './layouts/homepage/LibraryServices';
 import { Footer } from './layouts/footer/Footer';
+import { HomePage } from './layouts/homepage/homepage';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heros/>
-      <LibraryServices/>
+      <HomePage/>
       <Footer/>
     </div>
   );
