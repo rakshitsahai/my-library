f45d83f080953060eb475bf7953f4dbfa965c597
Rakshit <rakshit@ad.infosys.com>
2023-06-02 00:39:26 +0530

    added pagination to SearchBooksPage component

--- src/layouts/searchbookspage/SearchBooksPage.tsx
+++ src/layouts/searchbookspage/SearchBooksPage.tsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from 'react';
 import BookModel from '../../models/BookModel';
 import { SpinnerLoading } from '../utils/SpinnerLoading';
 import { SearchBook } from './components/SearchBook';
+import { Pagination } from '../utils/Pagination';
 
 export const SearchBooksPage = () => {
 
     const [books, setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [booksPerPage] = useState(5);
+    const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
         const fetchBooks = async () => {
             const baseUrl: string = "http://localhost:8080/api/books";
 
-            let url: string = `${baseUrl}?page=0&size=5`;
+            let url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
 
             const response = await fetch(url);
 
@@ -24,6 +29,9 @@ export const SearchBooksPage = () => {
             const responseJson = await response.json();
 
             const responseData = responseJson._embedded.books;
+
+            setTotalAmountOfBooks(responseJson.page.totalElements);
+            setTotalPages(responseJson.page.totalPages);
 
             const loadedBooks: BookModel[] = [];
 
@@ -48,7 +56,7 @@ export const SearchBooksPage = () => {
             setHttpError(error.message);
         })
         window.scrollTo(0, 0);
-    }, []);
+    }, [currentPage]);
 
     if (isLoading) {
         return (
@@ -63,6 +71,14 @@ export const SearchBooksPage = () => {
             </div>
         )
     }
+
+    const indexOfLastBook: number = currentPage * booksPerPage;
+    const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+    let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ?
+        booksPerPage * currentPage : totalAmountOfBooks;
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
     return (
         <div>
             <div className='container'>
@@ -71,7 +87,7 @@ export const SearchBooksPage = () => {
                         <div className='col-6'>
                             <div className='d-flex'>
                                 <input className='form-control me-2' type='search'
-                                    placeholder='Search' aria-labelledby='Search'/>
+                                    placeholder='Search' aria-labelledby='Search' />
                                 <button className='btn btn-outline-success'>
                                     Search
                                 </button>
@@ -114,15 +130,18 @@ export const SearchBooksPage = () => {
                             </div>
                         </div>
                     </div>
-                            <div className='mt-3'>
-                                <h5>Number of results: (22)</h5>
-                            </div>
-                            <p>
-                                1 to 5 of 22 items:
-                            </p>
-                            {books.map(book => (
-                                <SearchBook book={book} key={book.id} />
-                            ))}
+                    <div className='mt-3'>
+                        <h5>Number of results: ({totalAmountOfBooks})</h5>
+                    </div>
+                    <p>
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+                    </p>
+                    {books.map(book => (
+                        <SearchBook book={book} key={book.id} />
+                    ))}
+                    {totalPages > 1 &&
+                        <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
+                    }
                 </div>
             </div>
         </div>
