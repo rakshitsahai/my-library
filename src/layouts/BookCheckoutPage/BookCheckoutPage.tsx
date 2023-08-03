2a29d49fae8d92331cd90b9e9d96a96650f11bf1
Rakshit <rakshit@ad.infosys.com>
2023-08-03 18:14:19 +0530

    implemented StarsReview

--- src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../utils/SpinnerLoading";
+import { StarsReview } from "../utils/StarsReview";
 
 export const BookCheckoutPage = () => {
 
@@ -73,6 +74,7 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className='text-primary'>{book?.author}</h5>
                             <p className='lead'>{book?.description}</p>
+                            <StarsReview rating={4} size={32} />
                         </div>
                     </div>
                 </div>
